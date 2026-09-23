ac935a1cbf355ef5fa2da55b1921f004d8276e6b
Zulayha Darries <zudarcpt025@student.wethinkcode.co.za>
2026-09-24 01:23:39 +0200

    add: setter/getters for stage <- to be used for setting/getting the stage transit is in

---
+++ transit-service/src/main/java/co/wethinkcode/logisticsconnect/StageResponse.java
@@ -0,0 +1,17 @@
+package co.wethinkcode.logisticsconnect;
+
+public class StageResponse {
+
+    private int stage;
+
+    public StageResponse() {
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public void setStage(int stage) {
+        this.stage = stage;
+    }
+}
